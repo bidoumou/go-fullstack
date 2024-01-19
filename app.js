b11a98a2a0ea1464fa8ffd016c14f025e13dae3f
bidoumou <williamlg@free.fr>
2024-01-19 16:56:42 +0100

    Chapitre 2 terminé

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
 
 const app = express();
+mongoose.connect('mongodb+srv://william:bonjour@cluster0.1gzdstd.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -15,33 +23,37 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
-    next();
+    thing.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(thing => res.status(200).json({message: 'Objet modifié'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+        .then(thing => res.status(200).json({message: 'Objet supprimé'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({error}));
 });
 
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'ouefvefb',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: '',
-            price: 4900,                                        // les prix sont en centimes
-            userId: 'ojfsvnvre',
-        },
-        {
-            _id: 'ethrthbrt',
-            title: 'Mon second objet',
-            description: 'Les infos de mon second objet',
-            imageUrl: '',
-            price: 2900,
-            userId: 'ojfsvnvtrbgbre',
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({error}));
 });
 
 module.exports = app;
